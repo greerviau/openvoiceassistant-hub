0f8ba9de2ade1db5ccd7e4922f93fb09de5c516f
Greer <gviau2@gmail.com>
2022-08-08 22:22:45 -0400

    Update synthesizer.py

--- synthesizer.py
+++ synthesizer.py
@@ -4,6 +4,8 @@ import numpy as np
 
 import torch
 
+from scipy.io.wavfile import write
+
 import grad_tts.params as params
 from grad_tts.model import GradTTS
 from grad_tts.text import text_to_sequence, cmudict
@@ -16,7 +18,7 @@ from grad_tts.hifi_gan.models import Generator as HiFiGAN
 
 HIFIGAN_CONFIG = './grad_tts/checkpts/hifigan-config.json'
 HIFIGAN_CHECKPT = './grad_tts/checkpts/hifigan.pt'
-GRADTTS_CHECKPT = './grad_tts/checkpts/grad_440.pt'
+GRADTTS_CHECKPT = './grad_tts/checkpts/grad_2250.pt'
 
 class Synthesizer:
     def __init__(self):
@@ -45,7 +47,8 @@ class Synthesizer:
 
     def synthesize_text(self, text):
         with torch.no_grad():
-            x = torch.LongTensor(intersperse(text_to_sequence(text, dictionary=self.cmu), len(symbols))).cuda()[None]
+            seq = text_to_sequence(text, dictionary=self.cmu)
+            x = torch.LongTensor(intersperse(seq, len(symbols))).cuda()[None]
             x_lengths = torch.LongTensor([x.shape[-1]]).cuda()
             
             t = dt.datetime.now()
@@ -56,4 +59,5 @@ class Synthesizer:
 
             audio = (self.vocoder.forward(y_dec).cpu().squeeze().clamp(-1, 1).numpy() * 32768).astype(np.int16)
 
+            write(f'./output.wav', 22050, audio)
             return audio
