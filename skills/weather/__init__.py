f9d5c33bc5630628d24421a1a6ac598d2ed6e41f
Greer <gviau2@gmail.com>
2022-08-21 00:56:29 -0400

    working on skill handling

---
+++ skills/weather/__init__.py
@@ -0,0 +1,29 @@
+def weather(context):
+    command = context['command']
+    addr = context['addr'] if 'addr' in context else ''
+
+    context['response'] = 'Not implemented'
+
+def sky(context):
+    command = context['command']
+    addr = context['addr'] if 'addr' in context else ''
+
+    context['response'] = 'Not implemented'
+
+def air(context):
+    command = context['command']
+    addr = context['addr'] if 'addr' in context else ''
+
+    context['response'] = 'Not implemented'
+
+def temperature(context):
+    command = context['command']
+    addr = context['addr'] if 'addr' in context else ''
+
+    context['response'] = 'Not implemented'
+
+def ocean(context):
+    command = context['command']
+    addr = context['addr'] if 'addr' in context else ''
+
+    context['response'] = 'Not implemented'
