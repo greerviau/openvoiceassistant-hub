07551f6661aff95c06526fbbf7e9a1f474d69b48
Greer <gviau2@gmail.com>
2022-09-28 10:51:27 -0400

    Update api.py

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,202 +1,135 @@
-from fastapi import APIRouter, HTTPException, Depends
-from src.models import *
+import fastapi
+from fastapi.encoders import jsonable_encoder
 
-import os
-import json
-import requests
+from backend.ova import OpenVoiceAssistant
+from backend.src.models import *
 
-from src.models import *
-from utils.audio import audio_data_to_b64, wave_file_from_b64_encoded
-from utils.nlp import clean_text
+def create_app(ova: OpenVoiceAssistant):
 
-from managers.config_manager import ConfigManager
-from managers.node_manager import NodeManager
-from managers.skill_manager import SkillManager
+    router = fastapi.APIRouter(prefix="/api")
 
-from services.transcriber import Transcriber
-from services.classifier import Classifier
-from services.nlp_extractor import NLPExtractor
-from services.synthesizer import Synthesizer
+    @router.get('/')
+    async def index():
+        return {"Success"}
 
-config_manager = ConfigManager()
-node_manager = NodeManager(config_manager)
-skill_manager = SkillManager(config_manager)
+    # SKILLS
 
-transcriber = Transcriber(config_manager)
-classifier = Classifier(config_manager)
-nl_extractor = NLPExtractor()
-synth = Synthesizer(config_manager)
+    @router.get('/skills/available')
+    async def get_available_skills():
+        return ova.get_component('skillset').available_skills
 
-title = config_manager.get('title')
-engage_delay = config_manager.get('engage_delay')
+    @router.get('/skills/active')
+    async def get_active_skills():
+        return ova.get_component('skillset').imported_skills
 
-router = APIRouter(prefix="/api")
-
-@router.get('/')
-async def index():
-    return {"Success"}
-
-# SKILLS
-
-@router.put('/skills')
-async def put_skill(skill: str):
-    if skill_manager.is_skill_imported(skill):
-        if skill in os.listdir('skills'):
-            skill_config_path = f'skills/{skill}/config.json'
-            if os.path.exists(skill_config_path):
-                skill_config = json.load(open(skill_config_path))
-            else:
-                skill_config = {}
-            skill_manager.add_skill(skill, skill_config)
-
-            return skill_config
-        else:
-            raise HTTPException(
-                        status_code=404,
-                        detail='Skill does not exist',
-                        headers={'X-Error': 'There goes my error'})
-    else:
-        raise HTTPException(
-                    status_code=404,
-                    detail='Skill does not exist',
-                    headers={'X-Error': 'Skill is already imported'})
-
-@router.get('/skills/available')
-async def get_available_skills():
-    return os.listdir('skills')
-
-@router.get('/skills/active')
-async def get_active_skills():
-    return skill_manager.list_imported_skills()
-
-@router.get('/skills/config/{skill}')
-async def get_skill_config(skill: str):
-    if skill_manager.is_skill_imported(skill):
-        raise HTTPException(
-                    status_code=404,
-                    detail='Skill not imported',
-                    headers={'X-Error': 'There goes my error'})
-    else:
-        return skill_manager.get_skill_config(skill)
-
-@router.put('/skills/config/')
-async def put_skill_config(skill: str):
-    if skill not in skill_manager.imported_skills:
-        raise HTTPException(
-                    status_code=404,
-                    detail='Skill not imported',
-                    headers={'X-Error': 'There goes my error'})
-    else:
-        return skill_manager.get_skill_config(skill)
-
-# NODES
-
-@router.put('/node/sync')
-async def sync(data: NodeInfo = None):
-    if not data:
-        raise HTTPException(
-                    status_code=404,
-                    detail='command invalid',
-                    headers={'X-Error': 'No data provided'})
-
-    node_manager.set_node(data.node_id, data)
-
-    return {"Success"}
-
-@router.get('/node/status')
-async def get_node_status():
-    node_status = {}
-    for node_id, values in node_manager.nodes.items():
-        address = values['address']
+    @router.get('/skills/config/{skill_id}')
+    async def get_skill_config(skill_id: str):
         try:
-            resp = requests.get(address+'/status')
-            if resp.status_code == 200:
-                node_status[node_id] = 'online'
-            else:
-                raise
-        except:
-            node_status[node_id] = 'offline'
+            return ova.get_component('skillset').get_skill_config(skill_id)
+        except RuntimeError as err:
+            raise fastapi.HTTPException(
+                        status_code=404,
+                        detail=repr(err),
+                        headers={'X-Error': f'{err}'})
 
-    return node_status
 
-@router.get('/node/status/{node_id}')
-async def get_node_status(node_id: str):
-    if node_id in node_manager.nodes:
-        return node_manager.get_node_config(node_id)
-    else:
-        raise HTTPException(
-                    status_code=404,
-                    detail='Node does not exist',
-                    headers={'X-Error': 'There goes my error'})
+    @router.post('/skills/{skill_id}')
+    async def post_skill(skill_id: str):
+        try:
+            ova.get_component('skillset').add_skill(skill_id)
+        except RuntimeError as err:
+            raise fastapi.HTTPException(
+                        status_code=404,
+                        detail=repr(err),
+                        headers={'X-Error': f'{err}'})
 
-# RESPOND
+    @router.post('/skills/')
+    async def post_skill(data: SkillConfig):
+        try:
+            ova.get_component('skillset').add_skill_config(data.skill_id, data.config)
+        except RuntimeError as err:
+            raise fastapi.HTTPException(
+                        status_code=404,
+                        detail=repr(err),
+                        headers={'X-Error': f'{err}'})
+                        
+    @router.put('/skills/config/')
+    async def put_skill_config(data: SkillConfig):
+        try:
+            ova.get_component('skillset').update_skill_config(data.skill_id, data.config)
+        except RuntimeError as err:
+            raise fastapi.HTTPException(
+                        status_code=404,
+                        detail=repr(err),
+                        headers={'X-Error': f'{err}'})
 
-@router.post('/respond/audio')
-async def respond_to_audio(data: RespondToAudio = None):
-    if not data:
-        raise HTTPException(
-                    status_code=404,
-                    detail='No data provided',
-                    headers={'X-Error': 'There goes my error'})
+    # NODES
 
-    audio_file = data.audio_file
-    samplerate = data.samplerate
-    callback = data.callback
-    node_id = data.node_id
-    time_sent = data.time_sent
-    last_time_engaged = data.last_time_engaged
+    @router.put('/node/sync')
+    async def sync(data: NodeInfo = None):
+        if not data:
+            raise fastapi.HTTPException(
+                        status_code=404,
+                        detail='command invalid',
+                        headers={'X-Error': 'No data provided'})
+        try:
+            ova.node_manager.update_node_config(data.node_id, jsonable_encoder(data))
+        except Exception as err:
+            raise fastapi.HTTPException(
+                        status_code=404,
+                        detail=repr(err),
+                        headers={'X-Error': f'{err}'})
+
+    @router.get('/node/status')
+    async def get_node_status():
+        try:
+            return ova.node_manager.get_node_status()
+        except RuntimeError as err:
+            raise fastapi.HTTPException(
+                        status_code=404,
+                        detail=repr(err),
+                        headers={'X-Error': f'{err}'})
+
+    @router.get('/node/status/{node_id}')
+    async def get_node_status(node_id: str):
+        try:
+            return ova.node_manager.get_node_status()
+        except RuntimeError as err:
+            raise fastapi.HTTPException(
+                        status_code=404,
+                        detail=repr(err),
+                        headers={'X-Error': f'{err}'})
+
+    @router.get('/node/config/{node_id}')
+    async def get_node_status(node_id: str):
+        try:
+            return ova.node_manager.get_node_config(node_id)
+        except RuntimeError as err:
+            raise fastapi.HTTPException(
+                        status_code=404,
+                        detail=repr(err),
+                        headers={'X-Error': f'{err}'})
+
+    # RESPOND
+
+    @router.post('/respond/audio')
+    async def respond_to_audio(data: RespondToAudio):
+
+        context = {}
+
+        context['audio_file'] = data.audio_file
+        context['samplerate'] = data.samplerate
+        context['callback'] = data.callback
+        context['node_id'] = data.node_id
+        context['time_sent'] = data.time_sent
+        context['last_time_engaged'] = data.last_time_engaged
+
+        ova.run_pipeline('transcriber','understander', 'skillset', 'synthesizer', context=context)
+
+        return context
+
     
-    context = {}
+    app = fastapi.FastAPI()
+    app.include_router(router)
 
-    wave_file = wave_file_from_b64_encoded(audio_file)
-    command = transcriber.transcribe(wave_file, samplerate)
-
-    delta_time = time_sent - last_time_engaged
-
-    if command:
-        if title in command or delta_time < engage_delay:
-            cleaned_command = clean_text(command)
-            context['command'] = command
-            context['cleaned_command'] = cleaned_command
-
-            print(f'Command: {cleaned_command}')
-
-            skill, action, conf = classifier.predict_intent(cleaned_command)
-            context['skill'] = skill
-            context['action'] = action
-            context['conf'] = conf
-
-            print(f'Skill: {skill}')
-            print(f'Action: {action}')
-            print(f'Conf: {conf}')
-
-            nl_extractor.extract_from_command(cleaned_command)
-
-            skill_manager.skill_action(context)
-
-            response = context['response']
-            print(f'Response: {response}')
-
-            if response:
-            
-                audio = synth.synthesize(' '+response+' ')  # The space ensures the first word will be pronounced
-                audio_base64 = audio_data_to_b64(audio)
-
-                context['audio_data'] = audio_base64
-
-                return context
-            else:
-                raise HTTPException(
-                        status_code=404,
-                        detail='No response',
-                        headers={'X-Error': 'There goes my error'})
-        else:
-            raise HTTPException(
-                        status_code=404,
-                        detail='Not engaged',
-                        headers={'X-Error': 'There goes my error'})
-    else:
-        raise HTTPException(
-                    status_code=404,
-                    detail='No command',
-                    headers={'X-Error': 'There goes my error'})
+    return app
