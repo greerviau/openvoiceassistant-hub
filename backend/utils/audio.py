ecf6da8c6038f61d3cedf4464e5b71bc75408938
greerviau <gviau2@gmail.com>
2022-10-10 19:57:13 +0000

    option to save audio_data

--- backend/utils/audio.py
+++ backend/utils/audio.py
@@ -2,10 +2,20 @@ import wave
 import base64
 from io import BytesIO
 
-def wave_file_from_b64_encoded(audio_file):
+def wave_file_from_b64_encoded(audio_file, save=False):
     audio_data = bytes(base64.b64decode(audio_file.encode('utf-8')))
-    file_like = BytesIO(audio_data)
-    wf = wave.open(file_like, 'rb')
+
+    if not save:
+        wave_file = BytesIO()
+    else:
+        wave_file = 'command.wav'
+
+    wf = wave.open(wave_file, 'wb')
+    wf.setnchannels(self.CHANNELS)
+    wf.setsampwidth(self.SAMPLEWIDTH)
+    wf.setframerate(self.RATE)
+    wf.writeframes(audio_data)
+
     return wf
 
 def audio_data_to_b64(audio_data):
