14a9c6092e670fd2cc4891addb874f8620d4568d
Greer <gviau2@gmail.com>
2023-03-25 18:38:04 -0400

    Update weather.py

--- backend/skills/weather/weather.py
+++ backend/skills/weather/weather.py
@@ -60,7 +60,7 @@ class Weather:
         for chunk in data["list"]:
             parsed_chunk = {}
 
-            dt = datetime.strptime(chunk["dt_txt"], "%y-%m-%d %H:%M:%S")
+            dt = datetime.strptime(chunk["dt_txt"], "%Y-%m-%d %H:%M:%S")
 
             parsed_chunk["datetime"] = dt
 
