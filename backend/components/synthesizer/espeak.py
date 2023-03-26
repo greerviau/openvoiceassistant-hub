7affbc4f256c16b82e2cd24a120c49b28b47d81c
Greer <gviau2@gmail.com>
2023-03-25 22:02:31 -0400

    Update espeak.py

--- backend/components/synthesizer/espeak.py
+++ backend/components/synthesizer/espeak.py
@@ -13,6 +13,7 @@ class Espeak:
     def synthesize(self, text: str, file_path: str):
         self.engine.save_to_file(text, file_path)
         self.engine.runAndWait()
+        self.engine.stop()
         audio_seg = pydub.AudioSegment.from_file(file_path)
         
         return audio_seg.raw_data, audio_seg.frame_rate, audio_seg.sample_width
