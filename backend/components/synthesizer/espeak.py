93bc5ee6df83d1b13b9734a60f504d240c5d5439
Greer <gviau2@gmail.com>
2023-03-25 22:10:44 -0400

    Update espeak.py

--- backend/components/synthesizer/espeak.py
+++ backend/components/synthesizer/espeak.py
@@ -8,12 +8,13 @@ from backend import config
 
 class Espeak:
     def __init__(self):
-        self.engine = pyttsx3.init()
+        self.tts_engine = pyttsx3.init()
 
     def synthesize(self, text: str, file_path: str):
-        self.engine.save_to_file(text, file_path)
-        self.engine.runAndWait()
-        self.engine.stop()
+        self.tts_engine.save_to_file(text, file_path)
+        self.tts_engine.runAndWait()
+        self.tts_engine.stop()
+        time.sleep(0.2)
         audio_seg = pydub.AudioSegment.from_file(file_path)
         
         return audio_seg.raw_data, audio_seg.frame_rate, audio_seg.sample_width
