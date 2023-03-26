9af63d2b1214918b002857b11cd753c8638d49ef
Greer <gviau2@gmail.com>
2023-03-25 21:56:20 -0400

    Update datetime.py

--- backend/skills/datetime/datetime.py
+++ backend/skills/datetime/datetime.py
@@ -1,26 +1,28 @@
 from typing import Dict
 from datetime import datetime
+import pytz
 
 class Datetime:
     def __init__(self, config: Dict):
         self.config = config
+        self.tz = pytz.timezone(config["timezone"])
 
     def date(self, context: Dict):
-        date = datetime.now().strftime("%B %d, %Y")
+        date = datetime.now(self.tz).strftime("%B %d, %Y")
 
         response = f"Today is {date}"
 
         return response
 
     def time(self, context: Dict):
-        time = datetime.now().strftime("%H:%M")
+        time = datetime.now(self.tz).strftime("%H:%M")
 
         response = f"It is {time}"
 
         return response
 
     def day_of_week(self, context: Dict):
-        dow = datetime.now().strftime('%A')
+        dow = datetime.now(self.tz).strftime('%A')
 
         response = f"It is {dow}"
 
@@ -30,4 +32,6 @@ def build_skill(config: Dict):
     return Datetime(config)
 
 def default_config():
-    return {}
+    return {
+        "timezone": "US/Eastern"
+    }
