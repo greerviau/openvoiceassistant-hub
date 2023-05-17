99c3d8972cfcd001ac1db2be290a9f2267014127
Greer <gviau2@gmail.com>
2023-05-17 13:51:12 -0400

    adding homeassistant.shopping_list skill

--- backend/skills/homeassistant/shopping_list/shopping_list.py
+++ backend/skills/homeassistant/shopping_list/shopping_list.py
@@ -10,21 +10,52 @@ class ShoppingList:
         host = config["host"]
         acccess_token = config["acccess_token"]
         self.headers = {"content-type": "application/json", "Authorization": f"Bearer {acccess_token}"}
-        self.shopping_list_api = f"http://{host}:8123/api/shopping_list"
+        self.api = f"http://{host}:8123/api"
 
-    def add_to_shopping_list(self, config: Dict):
-        return "Not implemented"
+    def add_to_shopping_list(self, context: Dict):
+        pos_info = context["pos_info"]
+        try:
+            item_to_add = pos_info["MOD_OBJECT"] if "MOD_OBJECT" in pos_info else pos_info["OBJECT"][0]
+        except KeyError:
+            return "Please provide an item to add to the shopping list"
+        
+        body = {
+            "name": item_to_add
+        }
+        
+        resp = requests.post(f"{self.api}/services/shopping_list/add_item", headers=self.headers, json=body)
+        if resp.status_code == 200:
+            return f"Adding {item_to_add} to your shopping list"
+        
+        return f"Failed to add {item_to_add} to your shopping list"
 
-    def remove_from_shopping_list(self, config: Dict):
-        return "Not implemented"
+    def remove_from_shopping_list(self, context: Dict):
+        pos_info = context["pos_info"]
+        try:
+            item_to_remove = pos_info["MOD_OBJECT"] if "MOD_OBJECT" in pos_info else pos_info["OBJECT"][0]
+        except KeyError:
+            return "Please provide an item to add to the shopping list"
+        
+        body = {
+            "name": item_to_remove
+        }
+        
+        resp = requests.post(f"{self.api}/services/shopping_list/remove_item", headers=self.headers, json=body)
+        if resp.status_code == 200:
+            return f"Removing {item_to_remove} to your shopping list"
+        
+        return f"Failed to remove {item_to_remove} to your shopping list"
 
-    def read_shopping_list(self, config: Dict):
-        resp = requests.get(self.shopping_list_api, headers=self.headers)
+    def read_shopping_list(self, context: Dict):
+        resp = requests.get(f"{self.api}/shopping_list", headers=self.headers)
         if resp.status_code == 200:
             items = resp.json()
             item_names = [item["name"] for item in items]
-            if any(item_names):
-                return f"You have {', '.join(item_names)} on your shopping list"
+            last_item = item_names.pop(-1)
+            if any(item_names) and last_item:
+                return f"You have {','.join(item_names)} and {last_item} on your shopping list"
+            elif last_item:
+                return f"You only have {last_item} on your shopping list"
             else:
                 return "You dont have anything on your shopping list"
         return "I could not access a shopping list"
