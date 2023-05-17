59edc87bd876409bd8d845bcfbc0c69792fdf986
Greer <gviau2@gmail.com>
2023-05-17 13:50:54 -0400

    adding homeassistant.lights skill

--- backend/skills/homeassistant/lights/lights.py
+++ backend/skills/homeassistant/lights/lights.py
@@ -1,4 +1,5 @@
 from typing import Dict
+import requests
 
 from backend import config
 
@@ -6,13 +7,62 @@ class Lights:
 
     def __init__(self, config: Dict, ova: 'OpenVoiceAssistant'):
         self.config = config
+        host = config["host"]
+        acccess_token = config["acccess_token"]
+        self.headers = {"content-type": "application/json", "Authorization": f"Bearer {acccess_token}"}
+        self.api = f"http://{host}:8123/api"
 
-    def light_on(self, config: Dict):
-        pass
+    def light_on(self, context: Dict):
+        pos_info = context["pos_info"]
 
-    def light_off(self, config: Dict):
-        pass
+        body = {
+            "entity_id": self.get_light_id(pos_info)
+        }
+        
+        light_description = pos_info["NOUN_CHUNKS"][0]
+        
+        resp = requests.post(f"{self.api}/services/light/turn_on", headers=self.headers, json=body)
+        if resp.status_code == 200:
+            return f"Turning on {light_description}"
+        
+        return f"Failed to turn on {light_description}"
 
+    def light_off(self, context: Dict):    
+        pos_info = context["pos_info"]
+
+        body = {
+            "entity_id": self.get_light_id(pos_info)
+        }
+        
+        light_description = pos_info["NOUN_CHUNKS"][0]
+        
+        resp = requests.post(f"{self.api}/services/light/turn_off", headers=self.headers, json=body)
+        if resp.status_code == 200:
+            return f"Turning off {light_description}"
+        
+        return f"Failed to turn off {light_description}"
+    
+    def get_light_id(self, pos_info):
+        try:
+            light = pos_info["COMP"][0]
+        except KeyError:
+            return "Please provide a light to turn on"
+        
+        lights = self.get_lights()
+
+        try:
+            light_id = [l for l in lights if light in l][0]
+        except:
+            raise RuntimeError("Could not find the light specified")
+
+        return light_id
+    
+    def get_lights(self):
+        resp = requests.get(f"{self.api}/states", headers=self.headers)
+        if resp.status_code == 200:
+            entities = resp.json()
+            return [entity["entity_id"] for entity in entities if "light" in entity["entity_id"]]
+        raise RuntimeError("Failed to get list of light entites")
 
 def build_skill(config: Dict, ova: 'OpenVoiceAssistant'):
     return Lights(config, ova)
