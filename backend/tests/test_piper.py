14dc880aa764f68f74a75859b53e0b583617ac97
Greer <gviau2@gmail.com>
2023-12-01 19:01:37 -0500

    Update test_piper.py

--- backend/tests/test_piper.py
+++ backend/tests/test_piper.py
@@ -2,4 +2,4 @@ from piper.download import ensure_voice_exists, find_voice, get_voices
 from piper import PiperVoice
 
 voices_info = get_voices('./piper_voices')
-print(voices_info)
+print(voices_info.keys())
