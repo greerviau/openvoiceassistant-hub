fa05f229da58bd1e3254d5f1e001f0cc045837b9
Greer <gviau2@gmail.com>
2023-12-02 22:37:46 -0500

    Update schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,6 +1,11 @@
 import typing
 
 class Context(typing.TypedDict):
+    node_id: str
+    node_callback: str
+    time_sent: float
+    engaged: bool
+    last_time_engaged: float
     command_audio_data_str: str
     command_audio_data_bytes: bytes
     command_audio_sample_rate: int
@@ -10,22 +15,18 @@ class Context(typing.TypedDict):
     command: str
     cleaned_command: str
     encoded_command: str
-    node_callback: str
-    node_id: str
-    engaged: bool
-    time_sent: float
-    last_time_engaged: float
-    time_to_run_pipeline: float
+    pos_info: typing.Dict
+    skill: str
+    action: str
+    conf: float
     time_to_transcribe: float
-    time_to_synthesize: float
     time_to_understand: float
+    time_to_action: float
+    time_to_synthesize: float
+    time_to_run_pipeline: float
     response: str
     response_audio_data_str: str
     response_audio_data_bytes: bytes
     response_audio_sample_rate: str
     response_audio_sample_width: str
     response_audio_file_path: str
-    skill: str
-    action: str
-    conf: float
-    pos_info: typing.Dict
