dc85753feeac08d2edcf98c8e6593f644ff3c386
Greer <gviau2@gmail.com>
2023-12-09 21:41:03 -0500

    fixing day of month formatting

--- backend/utils/formatting.py
+++ backend/utils/formatting.py
@@ -2,21 +2,47 @@ from datetime import datetime
 import inflect
 
 def format_readable_date(dt):
+    DAY_OF_MONTH = {
+        1: 'first', 
+        2: 'second', 
+        3: 'third', 
+        4: 'fourth', 
+        5: 'fifth', 
+        6: 'sixth', 
+        7: 'seventh', 
+        8: 'eighth', 
+        9: 'ninth', 
+        10: 'tenth', 
+        11: 'eleventh', 
+        12: 'twelfth', 
+        13: 'thirteenth', 
+        14: 'fourteenth', 
+        15: 'fifteenth', 
+        16: 'sixteenth', 
+        17: 'seventeenth', 
+        18: 'eighteenth', 
+        19: 'nineteenth', 
+        20: 'twentieth', 
+        21: 'twenty first', 
+        22: 'twenty second', 
+        23: 'twenty third', 
+        24: 'twenty fourth', 
+        25: 'twenty fifth', 
+        26: 'twenty sixth', 
+        27: 'twenty seventh', 
+        28: 'twenty eighth', 
+        29: 'twenty ninth', 
+        30: 'thirtieth', 
+        31: 'thirty first'
+    }
+    
     month = dt.strftime("%B")
-    day = dt.strftime("%d").replace(" 0", " ")
+    day = dt.strftime(" %d").replace(" 0", "")
     year = dt.strftime("%Y")
 
     inf = inflect.engine()
 
-    if int(day) in [1, 21, 31]:
-        day = f"{day}st"
-    elif int(day) in [2, 22]:
-        day = f"{day}nd"
-    elif int(day) in [3, 23]:
-        day = f"{day}rd"
-    else:
-        day = f"{day}th"
-        
+    day = DAY_OF_MONTH[int(day)]
     century = inf.number_to_words(int(year[:2])).replace("-", " ")
     decade = inf.number_to_words(int(year[2:])).replace("-", " ")
 
