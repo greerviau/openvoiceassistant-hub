0b912842e11d9f594f2e23e381cb21c963b6d7d5
Greer <gviau2@gmail.com>
2023-12-13 13:08:41 -0500

    revert

--- scripts/start_backend.sh
+++ scripts/start_backend.sh
@@ -2,6 +2,4 @@
 
 source ./env/bin/activate
 
-export LD_LIBRARY_PATH=`python3 -c 'import os; import nvidia.cublas.lib; import nvidia.cudnn.lib; print(os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__))'`
-
 python -m backend
