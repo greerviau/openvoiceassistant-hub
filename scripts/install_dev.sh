ea1332050a87246b2968ac6e06d84eec8fcb5362
greerviau <gviau2@gmail.com>
2023-12-15 19:05:18 +0000

    Update install script

--- scripts/install_dev.sh
+++ scripts/install_dev.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-apt install -y python3-pip python3.11-dev python3.11-venv python3.11-dev nginx wget git bash systemd ffmpeg espeak libespeak-dev sed npm
+apt install -y python3-pip python3-dev python3-venv nginx wget git bash systemd ffmpeg espeak libespeak-dev sed npm
 
 CWD=$(pwd)
 
 echo $CWD
 
-python3.11 -m venv $CWD/env
+python3 -m venv $CWD/env
 
 source $CWD/env/bin/activate
 
