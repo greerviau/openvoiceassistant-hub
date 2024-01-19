56b1b0599fd00c13b7847d073e3acd4e340b6301
greerviau <gviau2@gmail.com>
2024-01-19 00:00:53 +0000

    Add debug port

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,11 +1,17 @@
 import uvicorn
-from backend.src.api import create_app
+import click
 
+from backend.src.api import create_app
 from backend.ova import OpenVoiceAssistant
 
-if __name__ == '__main__':
+@click.command()
+@click.option("--debug", is_flag=True)
+def main(debug):
     ova = OpenVoiceAssistant()
 
     app = create_app(ova)
-    uvicorn.run(app, host='0.0.0.0', port=5010)
+    uvicorn.run(app, host='0.0.0.0', port=5010 if debug else 7123)
     print("running")
+
+if __name__ == '__main__':
+    main()
