d71e8044502df9318a7f55fc3942e538ac5ba885
Greer <gviau2@gmail.com>
2024-02-25 14:39:21 -0500

    trying to fix whisper

--- core/components/transcriber/whisper.py
+++ core/components/transcriber/whisper.py
@@ -11,17 +11,20 @@ class Whisper:
         self.ova = ova
         model_size = config.get(Components.Transcriber.value, 'config', 'model_size')
         use_gpu = config.get(Components.Transcriber.value, 'config', 'use_gpu')
+        self.use_vad = config.get(Components.Transcriber.value, 'config', 'use_vad')
         use_gpu = torch.cuda.is_available() and use_gpu
         config.set(Components.Transcriber.value, 'config', 'use_gpu', use_gpu)
 
         device = "cuda" if use_gpu else "cpu"
 
-        self.model = WhisperModel(model_size, device=device, compute_type="float32" if use_gpu else "int8")
+        self.model = WhisperModel(model_size, device=device, compute_type="float16" if use_gpu else "int8")
 
     def transcribe(self, context: Context):
 
         file_path = context['command_audio_file_path']
-        segments, _ = self.model.transcribe(file_path, beam_size=5)
+        segments, _ = self.model.transcribe(file_path, 
+                                            vad_filter=self.use_vad, 
+                                            vad_parameters=dict(min_silence_duration_ms=500))
         segments = list(segments)
         #print(segments)
         return " ".join([segment.text for segment in segments])
